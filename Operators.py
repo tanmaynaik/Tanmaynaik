1997421a151950a5e371db7d034c65aba80814c3
tanmaynaik <68956673+tanmaynaik@users.noreply.github.com>
2021-08-12 11:59:46 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,20 @@
+x = 2
+y = 4
+
+#Addition
+print (x + y)
+
+#Subtraction
+print (x - y)
+
+#Multiplication
+print (x * y)
+
+#Division
+print (y / x)
+
+#Modulo
+print (y % x)
+
+#Power
+print (y ** x)
