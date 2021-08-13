b1db9744d0af07dcb4875be0fda2cdd70d905950
tanmaynaik <68956673+tanmaynaik@users.noreply.github.com>
2021-08-13 13:22:12 +0530

    Add files via upload

---
+++ AllOperators.py
@@ -0,0 +1,56 @@
+a = 20
+b = 10
+
+# Arithmetic Operator
+print("a + b = ",a+b)
+
+print("a - b = ",a-b)
+
+print("a * b = ",a*b)
+
+print("a / b = ",a/b)
+
+# Comparison Operator
+print("a < b",a<b)
+
+print("a > b",a>b)
+
+print("a <= b",a<=b)
+
+print("a >= b",a>=b)
+
+print("a == b",a==b)
+
+print("a != b",a!=b)
+
+# Logical Operator
+x = True
+y = False
+
+print("x and y is",x & y)
+
+print("x or y is",x or y)
+
+print("not y is",not y)
+
+# Identity Operator
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+
+print("x1 is not y1 ",x1 is not y1)
+
+print("x2 is y2",x2 is y2)
+
+# Membership Operator
+x = 'Hello world'
+y = {1:'a',2:'b'}
+
+print("H in x",'H' in x)
+
+print("hello not in x",'hello' not in x)
+
+print("1 in y",1 in y)
+
+print("a in y",'a' in y)
