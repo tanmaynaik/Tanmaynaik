2349a44d3f88ce5c0c47d7c0ddfc5fbddd78eb0d
tanmaynaik <68956673+tanmaynaik@users.noreply.github.com>
2021-08-23 14:23:23 +0530

    Update Exceptions.py

--- Aug23/Exceptions.py
+++ Aug23/Exceptions.py
@@ -1,34 +1,52 @@
 # Name Error
-# name = "Name Error"
-# print(nam)
+name = "Name Error"
+try:
+    print(nam)
+except:
+    print("This is Name error")
 
 # Overflow Error
 i=1
 f = 3.0**i
-for i in range(100):
-    print (i, f)
-    f = f ** 2
+try:
+    for i in range(100):
+        print (i, f)
+        f = f ** 2
+except:
+    print("This is Overflow Error")
 
-# Syntax Error
-print(()
+# # Syntax Error
+#     print(()
 
-#Identation Error
-    print("Identation error")
+# #Identation Error
+#     print("Identation error")
 
 # Value Error
 i="string"
-print(int(i))
+try:
+    print(int(i))
+except:
+    print("This is Value Error")
 
 # Type Error
-b = "String"
-print("Type error",b)
+b = 34
+try:
+    print("Type error" + b)
+except:
+    print("This is Type Error")
 
 # Unbound Local Error
 x = 5
-def local():
-    print(x)
-    x = 7
-local()
+try:
+    def local():
+        print(x)
+        x = 7
+    local()
+except:
+    print("This is Unbound Local Error")
 
 #Zero Division Error
-print(5/0)
+try:
+    print(5/0)
+except:
+    print("This is Zero Division Error")
